f10db56986e91ffa770443be4d1dd87c14ff6171
geni94 <eugen.yzeiri@gmail.com>
2021-08-03 09:52:45 +0200

    fixes for GET's

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import inventoriesController from '../controllers/inventoriesController.js';
-// import AuthMiddleware from '../services/middleware/auth.js';
+import AuthMiddleware from '../services/middleware/auth.js';
 
 const router = Router();
 
 router.post('/get-summaries-simple', inventoriesController.demoInventorySummaries);
 router.post('/get-inventories-aws', inventoriesController.getInventorySummaries);
+router.get('/get-inventories', AuthMiddleware.authenticateCalls, inventoriesController.getInventoriesFromDb);
 
 export default router;
