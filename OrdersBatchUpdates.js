64ce6a7295add2b19952713443f44fbbe8ff6dde
geni94 <eugen.yzeiri@gmail.com>
2021-09-17 22:42:19 +0200

    batch sync script mods

--- OrdersBatchUpdates.js
+++ OrdersBatchUpdates.js
@@ -3,39 +3,20 @@ import dotenv from 'dotenv';
 import OrderModel from './db/models/Order.js';
 import ConfigModel from './db/models/SpConfig.js';
 import SellingPartnerService from './services/sellingPartner.js';
+import DbService from './db/db.mjs';
 
 dotenv.config();
-
-// Connect to DB & Mongoose:
-const ClientConnection = () => {
-  const cloudUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-  return new Promise((resolve, reject) => {
-    mongoose
-      .connect(cloudUri, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      })
-      .then(() => {
-        console.log('DB is connected.');
-        return resolve();
-      })
-      .catch((err) => {
-        console.log('db error: ', err);
-        return reject(err);
-      });
-  });
-};
 
 const query = {
   userId: '',
 };
 
 const queryAllOrders = async () => {
-  await ClientConnection();
-  for await (const config of ConfigModel.find()) {
+  await DbService.ClientConnection();
+  const orders = await ConfigModel.find();
+  for await (const [idx, config] of orders.entries()) {
     let payload = {};
+
     query.userId = config.userId;
     if (config) {
       const { instance } = await SellingPartnerService.createUserInstance(config);
@@ -53,11 +34,15 @@ const queryAllOrders = async () => {
       const doc = await OrderModel.findOneAndUpdate(payload);
       if (doc) {
         console.log('ORDERS SYNCED!');
-        return true;
       }
     } catch (error) {
       console.log('ERROR SYNCING ORDERS: ', error);
       throw new Error(error);
+    }
+    if (idx === orders.length - 1) {
+      console.log('ALL ORDERS SYNCED!');
+      await DbService.CloseCurrentClient();
+      return true;
     }
   }
 };
